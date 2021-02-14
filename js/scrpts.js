12fcfc24f3e2b91a9699d700981ccba041415a4d
yuumiyasuoLilla <79060748+yuumiyasuoLilla@users.noreply.github.com>
2021-02-14 21:27:36 +0100

    Add files via upload

---
+++ js/scrpts.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+$(document).ready(function(){
+	$('.headerLong').hover(function(){
+		$('.dropdown-content').toggleClass('expand');
+	});
+});
+
+$(document).ready(function(){
+	$('.headerMedium').hover(function(){
+		$('.dropdown-content').toggleClass('expand');
+	});
+});
+
+$(document).ready(function(){
+	$('.headerLast').hover(function(){
+		$('.dropdown-content').toggleClass('expand');
+	});
+});
+
